f517b04e6b9c6830c2d1b223227166d573e2e62c
Yash-Marathe91 <yashmarathe4141@gmail.com>
2026-06-07 12:26:05 +0530

    chore: optimize custom cursor tracking physics

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -10,7 +10,7 @@ export default function CustomCursor() {
   const cursorX = useMotionValue(-100);
   const cursorY = useMotionValue(-100);
   
-  const springConfig = { damping: 25, stiffness: 300, mass: 0.5 };
+  const springConfig = { damping: 20, stiffness: 800, mass: 0.1 };
   const smoothX = useSpring(cursorX, springConfig);
   const smoothY = useSpring(cursorY, springConfig);
 
